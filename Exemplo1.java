b0d5a4324aa7595a56947943af77d4c382f5b213
danielsilva07 <daniel97silva.ds@gmail.com>
2024-02-20 18:02:52 -0300

    teste1

---
+++ Exemplo1.java
@@ -0,0 +1,19 @@
+public class Exemplo1 {
+
+
+    public static void main (String Args[]){
+
+
+        long idade2 = 2323232323l ;
+        byte idade = (byte) idade2;
+
+        System.out.println(idade2);
+        System.out.println(idade);
+
+
+        long notaFiscal1 = Long.valueOf(45454545);
+
+        System.out.println(notaFiscal1);
+
+    }
+}
